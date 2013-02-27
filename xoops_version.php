9c8808bfc697c085172b00dc733cc89bf63690c5
geekwright <richard@geekwright.com>
2013-02-26 20:43:03 -0600

    Add MEBKM block, bump rev to 1.3 RC

--- xoops_version.php
+++ xoops_version.php
@@ -13,7 +13,7 @@ if (!defined("XOOPS_ROOT_PATH")) die("Root path not defined");
 
 $modversion['name'] = _MI_QR_NAME;
 $modversion['dirname'] = basename( dirname( __FILE__ ) ) ;
-$modversion['version'] = '1.2';
+$modversion['version'] = '1.3';
 $modversion['description'] = _MI_QR_DESC;
 $modversion['author'] = "Richard Griffith (geekwright.com)";
 $modversion['credits'] = "QRcode scripts by Y.Swetake (swetake.com)";
@@ -29,7 +29,7 @@ else $modversion['image'] = "images/icon.png";
 $modversion['release_date']     = '2013/02/07';
 $modversion["module_website_url"] = "geekwright.com";
 $modversion["module_website_name"] = "geekwright.com";
-$modversion["module_status"] = "Final";
+$modversion["module_status"] = "RC";
 $modversion['min_php']='5.2';
 $modversion['min_xoops']='2.5';
 $modversion['system_menu'] = 1;
@@ -90,7 +90,7 @@ $modversion['blocks'][] = array(
   'description' => _MI_QR_HERE_DESC,
   'show_func' => 'b_qr_here_show',
   'edit_func' => 'b_qr_here_edit',
-  'options' => 'page,post_id,itemid,topic_id,forum,storyid|QR Code to access this page|0',
+  'options' => 'page,post_id,itemid,topic_id,forum,storyid,lid|QR Code to access this page|0',
   'template' => 'qr_block.html');
 
 $modversion['blocks'][] = array(
@@ -102,16 +102,23 @@ $modversion['blocks'][] = array(
   'options' => '',
   'template' => 'qr_block.html');
 
-  $modversion['blocks'][] = array(
+$modversion['blocks'][] = array(
   'file' => 'blocks.php',
   'name' => _MI_QR_HEREX,
   'description' => _MI_QR_HEREX_DESC,
   'show_func' => 'b_qr_here_show',
   'edit_func' => 'b_qr_here_edit',
-  'options' => 'page,post_id,itemid,topic_id,forum,storyid|QR Code to access this page|0',
+  'options' => 'page,post_id,itemid,topic_id,forum,storyid,lid|QR Code to access this page|0',
   'template' => 'qr_blockx.html');
 
-
+$modversion['blocks'][] = array(
+  'file' => 'blocks.php',
+  'name' => _MI_QR_BOOKMARK,
+  'description' => _MI_QR_BOOKMARK_DESC,
+  'show_func' => 'b_qr_bookmark_show',
+  'edit_func' => 'b_qr_bookmark_edit',
+  'options' => 'page,post_id,itemid,topic_id,forum,storyid,lid|QR Code bookmark for this page|0|0|'._MB_QR_MEBKM_LAUNCH;,
+  'template' => 'qr_block_mebkm.html');
 
 // Templates
 $modversion['templates'][1]['file'] = 'qr_index.html';
